99dab533d35088466fb146f542d54835466a52c1
Gustavo <gusstavocta@gmail.com>
2024-08-21 00:14:17 -0500

    editar curso

---
+++ Aplicacion/Cursos/Editar.cs
@@ -0,0 +1,51 @@
+﻿using MediatR;
+using Persistencia;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Aplicacion.Cursos
+{
+    public class Editar
+    {
+        public class Ejecuta : IRequest
+        {
+            public Guid CursoId { get; set; }
+            public string Titulo { get; set; }
+            public string Descripcion { get; set; }
+            public DateTime? FechaPublicacion { get; set; }
+        }
+
+        public class Manejador : IRequestHandler<Ejecuta>
+        {
+            private readonly CursosOnlineContext _context;
+            public Manejador(CursosOnlineContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Unit> Handle(Ejecuta request, CancellationToken cancellationToken)
+            {
+                
+                var curso = await _context.Curso.FindAsync(request.CursoId);
+                if(curso == null)
+                {
+                    throw new Exception("No se encontro el curso");
+                }
+                curso.Titulo = request.Titulo ?? curso.Titulo;
+                curso.Descripcion = request.Descripcion ?? curso.Descripcion;
+                curso.FechaPublicacion = request.FechaPublicacion ?? curso.FechaPublicacion;
+
+                var resultado = await  _context.SaveChangesAsync();
+                if(resultado > 0)
+                {
+                    return Unit.Value;
+                }
+                throw new Exception("No se guardaron los cambios en el curso");
+            }
+        }   
+    }
+}
