13b255c4a7f2d07c401bdf48b5a5e6f99c48fbe2
Gustavo <gusstavocta@gmail.com>
2024-08-28 23:51:37 -0500

    implementando JWT

--- Aplicacion/Seguridad/UsuarioData.cs
+++ Aplicacion/Seguridad/UsuarioData.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Aplicacion.Seguridad
 {
+    //Filtramos los datos que queremos mostrar del usuario al loguearse
     public class UsuarioData
     {
         public string NombreCompleto { get; set; }
